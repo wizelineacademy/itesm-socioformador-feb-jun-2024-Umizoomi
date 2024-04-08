41e05a6a6852fab0b21046decdd26b450ba0e642
Oscar Ortega <88897176+TheNullPower@users.noreply.github.com>
2024-04-08 16:09:53 -0600

    Revert "Documentos"

--- sara/src/Components/LoginForm/LoginForm.jsx
+++ sara/src/Components/LoginForm/LoginForm.jsx
@@ -1,25 +1,30 @@
-import React from 'react';
-import Input from '../Elements/Input';
-import './LoginForm.css';
+import React from "react";
+import './LoginForm.css'
+import { FaUser, FaLock } from "react-icons/fa";
 
 const LoginForm = () => {
-  return (
-    <div className="wrapper">
-      <form action="">
-        <h1>Login</h1>
-        <Input type="text" placeholder="Username" required />
-        <Input type="password" placeholder="Password" required />
-        <div className="remember-forgot">
-          <label>
-            <input type="checkbox" />Remember me
-          </label>
-          <a href="#">Forgot password?</a>
+    return (
+        <div className="wrapper">
+            <form action="">
+                <h1>Login</h1>
+                <div className="input-box">
+                    <input type="text" placeholder="Username" required />
+                    <FaUser className="icon" />
+                </div>
+                <div className="input-box">
+                    <input type="password" placeholder="Password" required />
+                    <FaLock className="icon" />
+                </div>
+
+                <div className="remember-forgot">
+                    <label><input type="checkbox" />Remember me</label>
+                    <a href="#">Forgot password?</a>
+                </div>
+
+                <button type="submit">Login</button>
+            </form>
         </div>
+    )
+}
 
-        <button type="submit">Login</button>
-      </form>
-    </div>
-  );
-};
-
-export default LoginForm;
+export default LoginForm
