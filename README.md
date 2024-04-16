83aa1f7a183fa80f279c99cc4b1ec43d42422140
CosmicBlue <A00833278@tec.mx>
2024-04-16 11:59:21 -0600

    Fixed project

--- README.md
+++
@@ -1,4 +0,0 @@
-# itesm-socioformador-feb-jun-2024-Umizoomi
-Socio formador ITESM - Wizeline semestre febrero-junio 2024
-
-# Rama test 3 
---
+++ saraia/app/login/page.tsx
@@ -0,0 +1,12 @@
+// pages/GradientPage.tsx
+
+const Login: React.FC = () => {
+    return (
+      <div className="h-screen flex justify-center items-center bg-gradient-login">
+        <h1 className="text-white font-bold text-4xl">Login </h1>
+      </div>
+    );
+  };
+  
+  export default Login;
+  
