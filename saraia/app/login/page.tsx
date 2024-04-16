cfce14872cce634a903167a341424366dfddc53a
CosmicBlue <A00833278@tec.mx>
2024-04-16 12:32:32 -0600

    Updated

---
+++ saraia/app/login/page.tsx
@@ -0,0 +1,21 @@
+
+
+const Login: React.FC = () => {
+  return (
+    <div className="bg-gradient-custom h-screen flex justify-center items-center">
+      <div id="LoginContainer" className='bg-slate-50 w-2/3 h-2/3 rounded-lg '>
+        <div id="Login-Left">
+
+        </div>
+        <div id="SeparationLine">
+          
+        </div>
+        <div id="LoginRightComponent">
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
