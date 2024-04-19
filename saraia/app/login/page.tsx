a55d91c99e9692012a23e45669b9080873bdf5cb
TheNullPower <oscaritoariel_23of@hotmail.com>
2024-04-18 19:34:23 -0600

    Component-1

--- saraia/app/login/page.tsx
+++ saraia/app/login/page.tsx
@@ -1,18 +1,19 @@
-
 import { Button } from "@/components/ui/button"
+import Image from "next/image";
 
 const Login: React.FC = () => {
   return (
-    <div className="bg-gradient-custom h-screen flex justify-center items-center">
-      <div id="LoginContainer" className='bg-slate-50 w-2/3 h-2/3 rounded-lg '>
-        <div id="Login-Left">
-
+    <div className="bg-gradient-custom h-screen flex justify-center items-center bg-gradient-to-b from-blue-950 to-cyan-500">
+      <div id="LoginContainer" className='bg-slate-50 w-2/3 h-5/6 rounded-lg flex'>
+        <div id="Login-Left" className="flex justify-center items-center">
+          <div className=" bg-slate-100 shadow-md h-2/3 w-2/3">
+            <Image src="/UmizoomiN.png" alt="EquipoUmizoomi" width={500} height={500} />
+          </div>
         </div>
-        <div id="SeparationLine">
+        <div id="SeparationLine" className="h-auto flex bg-gradient-to-b from-blue-400 to-blue-900 w-1 my-12">
           
         </div>
         <div id="LoginRightComponent">
-          <Button variant="outline">Button</Button>
 
         </div>
       </div>
