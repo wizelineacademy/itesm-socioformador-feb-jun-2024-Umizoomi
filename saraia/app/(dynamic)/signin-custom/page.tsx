ed4c7f09746c43340bd4bf84b98daebec51de862
CosmicBlue <A00833278@tec.mx>
2024-04-22 17:13:07 -0600

    Added custom button

---
+++ saraia/app/(dynamic)/signin-custom/page.tsx
@@ -0,0 +1,22 @@
+import SignOutBtnCustom from "@/components/signout-btn-custom";
+import SignInBtnCustom from "@/components/signin-btn-custom";
+import {auth} from "@/lib/auth"
+
+export default async function Page(){ 
+    const session = await auth();
+
+    if(session){ 
+        return (
+            <div>
+                <div>Signed in as: {session.user.name}</div>
+                <SignOutBtnCustom />
+            </div>
+        )
+    }
+
+    return <div>
+            <h1>Sign in Custom</h1>
+            <SignInBtnCustom provider={{id: "github", name: "Github"}} />    
+    </div>  
+
+}
