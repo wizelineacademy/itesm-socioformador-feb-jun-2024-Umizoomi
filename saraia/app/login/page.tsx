db8665a268629f318760213736c667853dc2a3ee
TheNullPower <oscaritoariel_23of@hotmail.com>
2024-04-22 10:39:33 -0600

    Update Login
    Modificación del Login

--- saraia/app/login/page.tsx
+++ saraia/app/login/page.tsx
@@ -4,15 +4,18 @@ import LoginForm from "../loginform/page";
 
 const Login: React.FC = () => {
   return (
-    <div className="bg-gradient-custom flex justify-center items-center h-screen bg-gradient-to-b from-blue-950 to-cyan-500">
-      <div id="LoginContainer" className='bg-slate-50 w-2/3 h-5/6 rounded-lg flex items-center justify-center gap-10'>
+    <div className="bg-gradient-custom h-screen flex justify-center items-center bg-gradient-to-b from-blue-950 to-cyan-500">
+      <div id="LoginContainer" className='bg-slate-50 w-2/3 h-5/6 rounded-lg flex'>
         <div id="Login-Left" className="flex justify-center items-center">
-            <Image src="/UmizoomiN.png" alt="EquipoUmizoomi" width={300 } height={300} />
+          <div className=" bg-slate-100 shadow-md h-2/3 w-2/3">
+            <Image src="/UmizoomiN.png" alt="EquipoUmizoomi" width={500} height={500} />
+          </div>
         </div>
-        <div id="SeparationLine" className="w-1 bg-gradient-to-b from-blue-400 to-blue-900 h-2/3 my-12">
-          
+        <div id="SeparationLine" className="h-auto flex bg-gradient-to-b from-blue-400 to-blue-900 w-1 my-12">
         </div>
-        <LoginForm />
+        <div id="Login-Right" className="flex justify-center items-center w-full lg:w-1/2 p-8">
+          <LoginForm />
+        </div>
       </div>
     </div>
   );
