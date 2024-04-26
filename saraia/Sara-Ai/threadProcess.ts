06374b4919a4f5802b45184c9ea6f12e2170bdd6
Cherry-Ayala <cherry.devservices@gmail.com>
2024-04-26 09:45:10 -0600

    run

--- saraia/Sara-Ai/threadProcess.ts
+++ saraia/Sara-Ai/threadProcess.ts
@@ -5,15 +5,26 @@ export const threadProcess = async(
     threadId: any, //cambiar al correcto no hay que ser nacos
     message: string,
     userId: string
-): Promise<any> => {await OpenAiInstance.beta.threads.create({
-    //@ts-ignore
-    threadId: threadId,
-    messages: [
-        {
-        role: "user",
-        content:message,
-        // Insertar el nuevo archivo
-        //attachments: [{ file_id: aapl10k.id, tools: [{ type: "file_search" }] }],
-    },
-    ],
-})};
+): Promise<any> => {
+    try{
+        await OpenAiInstance.beta.threads.create({
+        //@ts-ignore
+        threadId: threadId,
+        messages: [
+            {
+                role: "user",
+                content:message,
+                // Insertar el nuevo archivo
+                //attachments: [{ file_id: aapl10k.id, tools: [{ type: "file_search" }] }],
+            },
+        ],
+    })
+    const run = await OpenAiInstance.beta.threads.runs.createAndPoll(threadId.id, {
+        //@ts-ignore
+        assistant_id: assistant.id, //assistant aún no se crea, es placeholder
+    });
+
+
+}catch(error){
+        console.log('error')
+    }};
