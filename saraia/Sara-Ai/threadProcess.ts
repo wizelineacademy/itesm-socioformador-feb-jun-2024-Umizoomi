f04a30131aa12199d79ee1644770344d8a636dd4
Cherry-Ayala <cherry.devservices@gmail.com>
2024-05-20 11:41:51 -0600

    Code for assistant and vector store
    check error in updateAssistat.ts
    
    Wrote the same as official documentation, but marks a type error

--- saraia/Sara-Ai/threadProcess.ts
+++ saraia/Sara-Ai/threadProcess.ts
@@ -3,28 +3,27 @@ import { OpenAiInstance } from "./OpenAiInstance";
 
 export const threadProcess = async(
     threadId: any, //cambiar al correcto no hay que ser nacos
-    message: string,
+    messages: string,
     userId: string
 ): Promise<any> => {
     try{
-        await OpenAiInstance.beta.threads.create({
-        //@ts-ignore
-        threadId: threadId,
-        messages: [
+        const thread = await OpenAiInstance.beta.threads.create({})
+        const message = await OpenAiInstance.beta.threads.create(
+            //@ts-ignore
+            thread.id, //no se porque marca error, esta como en la documentación
             {
                 role: "user",
-                content:message,
-                // Insertar el nuevo archivo
-                //attachments: [{ file_id: aapl10k.id, tools: [{ type: "file_search" }] }],
-            },
-        ],
-    })
-    const run = await OpenAiInstance.beta.threads.runs.createAndPoll(threadId.id, {
-        //@ts-ignore
-        assistant_id: assistant.id, //assistant aún no se crea, es placeholder
-    });
+                content: messages
+
+            }
+        );
+        let run = await OpenAiInstance.beta.threads.runs.createAndPoll(threadId.id, {
+            //@ts-ignore
+            assistant_id: assistant.id, //assistant aún no se crea, es placeholder
+            instructions: "Address the user by its name and "
+        });
 
 
 }catch(error){
-        console.log('error')
+        console.log('error in thread')
     }};
--- saraia/Sara-Ai/updateAssistant.ts
+++ saraia/Sara-Ai/updateAssistant.ts
@@ -7,15 +7,16 @@ async function main() {
 
 
     // Crear un archivo para "entrenamiento"
-    /*
+    
     const file = await OpenAiInstance.files.create({
-        file: fs.createReadStream('pendiente'),
+        file: fs.createReadStream("./Sara-Ai/wizeline.v2.txt"),
         purpose: 'assistants',
     });
     console.log(file);
-    */
+    
 
     //Creación assistant
+    /*
     const assistant = await OpenAiInstance.beta.assistants.create({
         name: 'Sara1.0',
         instructions: "You are an Assistant named 'Sara', and your main goal is to grade workers in a company based on their peers and their own feedback about the project. The way you are going to assign their grade is based on the document information. You also need to asses a chemistry rank between employees, in order to asses their team preferences or their most efficient team possible; for this grade you will also use their own and their peers feedback, but now instead of taking a technical approach you should take a more 'friendlu' approach.",
@@ -36,3 +37,4 @@ async function main() {
 
 }
 
+main();
