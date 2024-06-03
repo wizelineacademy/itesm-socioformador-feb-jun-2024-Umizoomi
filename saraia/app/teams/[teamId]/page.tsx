fc36be63266bdac1cd4b17a048f2102e20167164
CosmicBlue <A00833278@tec.mx>
2024-06-02 19:16:13 -0600

    Added route via button to team site

---
+++ saraia/app/teams/[teamId]/page.tsx
@@ -0,0 +1,55 @@
+import {Miembro , columns } from "./columns"
+import { DataTable } from "./data-table"
+import Sidebar from "@/components/sidebar/Sidebar"
+
+type TeamName = {
+  teamName: string;
+};
+
+type UserTeamsProps = {
+  initialTeamNames: TeamName[];
+};
+
+const API_URL = '/api/teams';
+
+async function getData(TeamId : number): Promise<Miembro[]> {
+  try {
+    const response = await fetch(`${API_URL}?userId=${TeamId}`);
+    console.log(response);
+    if (!response.ok) {
+      throw new Error('Failed to fetch data');
+    }
+    const tabledata = await response.json();
+    
+    return tabledata;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return [];
+  }
+}
+
+
+
+export default async function Teams({
+    params,
+}: {
+  params: {teamId: number};
+}) {
+  const data = await getData()
+
+  return (
+    <div  id="container" className="flex ">
+        <Sidebar />
+        <div className=" flex-col flex gap-10 my-5 mx-10 flex-1">
+            
+            <h1 className="text-3xl font-sans font-bold">Team Details</h1>
+            <DataTable columns={columns} data={data} />
+
+        </div>
+    </div>
+
+
+  )
+}
+
+
--- saraia/app/teams/columns.tsx
+++ saraia/app/teams/columns.tsx
@@ -1,35 +1,40 @@
-"use client"
+"use client";
 
-import { ColumnDef } from "@tanstack/react-table"
+import { ColumnDef } from "@tanstack/react-table";
 import { MdArrowForwardIos } from "react-icons/md";
- 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
 
-// This type is used to define the shape of our data.
-// You can use a Zod schema here if you want.
+
 export type Projecto = {
-  nombre: string
+  nombre: string;
+  id: string;
+};
 
-}
+// Define a new functional component for the action button
+const ActionButton = ({ id }: { id: string }) => {
+  const router = useRouter();
+  const handleClick = () => {
+    router.push(`/teams/${id}`);
+  };
+
+  return (
+    <Button variant="outline" size="icon" onClick={handleClick}>
+      <MdArrowForwardIos className="h-4 w-4" />
+    </Button>
+  );
+};
 
 export const columns: ColumnDef<Projecto>[] = [
-    {
-        accessorKey: "nombre",
-        header: "Nombre",
-      },
+  {
+    accessorKey: "nombre",
+    header: "Nombre",
+  },
 
-    
-    {
-        id:"actions",
-        cell: ({row}) => {
-            const equipo = row.original
-
-            return (
-                <Button variant="outline" size="icon">
-                    <MdArrowForwardIos className="h-4 w-4" />
-                </Button>
-            )
-        }
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      return <ActionButton id={row.original.id} />;
     },
-
-]
+  },
+];
