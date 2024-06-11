6c2ceadad052a880304cd9b29afc73eda9469568
CosmicBlue <A00833278@tec.mx>
2024-06-10 20:07:02 -0600

    Added route for teammates and chat

--- saraia/app/api/teammates/route.ts
+++ saraia/app/api/teammates/route.ts
@@ -19,9 +19,7 @@ export async function GET(request: NextRequest) {
         const teamIdNumber = Number(teamId);
 
         // Perform the database query
-        const teamNames = await db.execute(sql`SELECT t2.id_user, t1.username, t3.title AS "job_title", t4.team_name, COALESCE(sub.overall_average, 0) AS overall_average FROM users t1 INNER JOIN userteamposition t2 ON t1.id_user = t2.id_user INNER JOIN jobtitles t3 ON t2.id_jobtitle = t3.id_jobtitle INNER JOIN team t4 ON t2.id_team = t4.id_team LEFT JOIN (SELECT id_user, (AVG(performance) + AVG(well_being) + AVG(flow) + AVG(communication) + AVG(proactivity) + AVG(collaboration) + AVG(efficiency) + AVG(satisfaction)) / 8 AS overall_average FROM feedback GROUP BY id_user) sub ON t2.id_user = sub.id_user WHERE t2.id_team = 21;
-        `);
-        console.log(teamNames)
+        const teamNames = await db.execute(sql`SELECT t2.id_user, t1.username, t3.title AS "job_title", t4.team_name, COALESCE(sub.overall_average, 0) AS overall_average FROM users t1 INNER JOIN userteamposition t2 ON t1.id_user = t2.id_user INNER JOIN jobtitles t3 ON t2.id_jobtitle = t3.id_jobtitle INNER JOIN team t4 ON t2.id_team = t4.id_team LEFT JOIN (SELECT id_user, (AVG("Performance") + AVG("well_being") + AVG("flow") + AVG("communication") + AVG("activity") + AVG(collaboration) + AVG(efficiency) + AVG(satisfaction)) / 8 AS overall_average FROM feedback GROUP BY id_user) sub ON t2.id_user = sub.id_user WHERE t2.id_team = 21;`);
 
         // Return the results as a JSON response
         return NextResponse.json(teamNames);
--- saraia/app/teams/[teamId]/columns.tsx
+++ saraia/app/teams/[teamId]/columns.tsx
@@ -2,8 +2,19 @@
 
 import { ColumnDef } from "@tanstack/react-table"
 import { MdArrowForwardIos } from "react-icons/md";
- 
+import { ArrowUpDown, ChevronDown, MoreHorizontal } from "lucide-react"
+
+
 import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
@@ -31,19 +42,29 @@ export const columns: ColumnDef<Miembro>[] = [
         accessorKey: "overall_avarage",
         header: "Grade",
       },
+      {
+        id: "actions",
+        enableHiding: false,
+        cell: ({ row }) => {
+          const payment = row.original
+     
+          return (
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant="ghost" className="h-8 w-8 p-0">
+                  <span className="sr-only">Open menu</span>
+                  <MoreHorizontal className="h-4 w-4" />
 
-    
-    {
-        id:"actions",
-        cell: ({row}) => {
-            const equipo = row.original
-
-            return (
-                <Button variant="outline" size="icon">
-                    <MdArrowForwardIos className="h-4 w-4" />
                 </Button>
-            )
-        }
-    },
-
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end">
+                <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                <DropdownMenuItem>Remove</DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>Edit member</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          )
+        },
+      }
 ]
