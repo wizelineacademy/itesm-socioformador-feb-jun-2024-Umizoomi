f89d4f2a4f2d1bfb02f8cfb3859e364516b8228d
ChalkyMuffin <adriancavazosguerra@gmail.com>
2024-06-19 11:12:54 -0600

    test

--- saraia/testing_A00833278/rendertestmodall.test.tsx
+++ saraia/testing_A00833278/rendertestmodall.test.tsx
@@ -1,6 +1,5 @@
 
 import '@testing-library/jest-dom'; 
-import userEvent from '@testing-library/user-event';
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import CreateTeamModal from '../components/Modals/CreateTeamModal';
